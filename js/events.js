47f296060df70c0dd5ccaf75da9cc8a131363458
Pete A <peter.ayresmusic@gmail.com>
2019-01-12 19:19:02 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -11,7 +11,7 @@ function frameIt(){
 };
 function pressIt(){
   $('form').on('keydown', function() {
-    if ($("input:first").val() === "g"){
+    if ($("input:first").val() === "G"){
           alert('G key was pressed down');
       }
     })
