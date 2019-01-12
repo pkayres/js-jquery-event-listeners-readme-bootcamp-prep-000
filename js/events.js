0f3eec8831d643be1406cc861202feac962321ae
Pete A <peter.ayresmusic@gmail.com>
2019-01-12 18:58:04 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -22,7 +22,7 @@ $('p').on("click", function (){
 $('img').on("load", function(){
   console.log('string');
   // $(.img).append('tasty');
-  };
+  }
 )
 $('form').on('keydown', function(key) {
   if(key.which == 71){
